1e6d6af21583bc9e31e289a2213c2c1654235cb6
Davide Bronzini <davide_bronzini@hotmail.com>
2022-03-01 15:43:50 +0100

    Initial commit

---
+++ src/MailerServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace IlBronza\Mailer;
+
+use Illuminate\Support\ServiceProvider;
+
+class MailerServiceProvider extends ServiceProvider
+{
+    /**
+     * Permailer post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'mailer');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'mailer');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/mailer.php', 'mailer');
+
+        // Register the service the package provides.
+        $this->app->singleton('mailer', function ($app) {
+            return new Mailer;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['mailer'];
+    }
+    
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole()
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/mailer.php' => config_path('mailer.php'),
+        ], 'mailer.config');
+
+        $this->publishes([
+            __DIR__.'/../resources/assets' => base_path('resources'),
+        ], 'mailer.assets');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/ilbronza'),
+        ], 'mailer.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/ilbronza'),
+        ], 'mailer.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/ilbronza'),
+        ], 'mailer.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
